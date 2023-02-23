b2d9749e0e4160c50c1c39bea87d5cb3d6be3077
Igor Polischuk <polischuk01@gmail.com>
2023-02-23 19:52:56 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,119 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        index: './ts/app.ts',
+    },
+    devtool: isDev ? 'source-map' : 'eval',
+    devServer: {
+        open: true,
+        hot: isDev,
+        port: 'auto',
+        static: {
+            directory: './src',
+            watch: true
+        }
+    },
+    output: {
+        filename: 'js/[name].[contenthash].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/[name].[ext]',
+        clean: true
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        }
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            template: './index.html',
+            inject: 'body',
+            watch: true
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{
+                    from: path.resolve(__dirname, 'src/assets/img'),
+                    to: path.resolve(__dirname, 'dist/assets/img'),
+                    noErrorOnMissing: true
+                },
+                {
+                    from: path.resolve(__dirname, 'src/svg/'),
+                    to: path.resolve(__dirname, 'dist/svg/'),
+                    noErrorOnMissing: true
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/style.[contenthash].css'
+        }),
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader',
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    ["autoprefixer"]
+                                ],
+                            },
+                        }
+                    },
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                debug: isDev,
+                                corejs: 3,
+                                useBuiltIns: "usage"
+                            }]
+                        ]
+                    }
+                }
+            }
+        ],
+    },
+};
