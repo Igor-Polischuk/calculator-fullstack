1df06c7705e9d133af74d3cc61583bdb24bc9c94
Igor Polischuk <polischuk01@gmail.com>
2023-03-15 20:18:14 +0200

    implemented showing result

--- src/ts/components/Calculator/views/CalculatorView.ts
+++ src/ts/components/Calculator/views/CalculatorView.ts
@@ -7,15 +7,30 @@ import { getCalculatorButtons } from './getCalculatorButtons';
 
 export class CalculatorView implements ICalculatorView {
     private mathInput = getMathInput()
+    private resultBlock = document.querySelector<HTMLDivElement>('#result-output')!
 
     constructor(private model: ICalculatorModel) {
         model.subscribe(CalculatorObserverEvent.Result, this.renderResult.bind(this))
+        model.subscribe(CalculatorObserverEvent.Error, this.renderError.bind(this))
         this.mathInput.domEl.focus()
         this.conectButtons()
     }
 
     private renderResult(newResult: number) {
+        this.resultBlock.classList.add('visible')
+        const formatedExpression = this.formatExpression(this.mathInput.value)
+        this.resultBlock.innerHTML = `<p class='showup'>${formatedExpression} = <b>${newResult}</b></p>`
         this.mathInput.value = newResult.toString()
+    }
+
+    private renderError(error: IError[]){
+        this.resultBlock.classList.add('visible')
+        const errorIndices = error.map(err => err.meta.errorIndex!)
+        const errorString = this.mathInput.value.split('').reduce<string>((errAcc, char, i) => {
+
+            return errAcc + ((errorIndices.includes(i)) ? `<span class='error'>${char}</span>` : char)
+        }, '')        
+        this.resultBlock.innerHTML = errorString
     }
 
     private conectButtons() {
@@ -28,6 +43,7 @@ export class CalculatorView implements ICalculatorView {
         })
 
         resultBtn.onClick(() => {
+            if(this.mathInput.value.trim().length === 0) return
             this.model.setExpression(this.mathInput.value)
         })
     }
@@ -42,9 +58,13 @@ export class CalculatorView implements ICalculatorView {
     }
 
     private formatExpression(expression: string) {
+        const regex = /(\^(\d+|\([^()]*\)))/g;
+
         return expression
             .replace(/\*/g, '×')
             .replace(/\//g, '÷')
             .replace(/\-/g, '−')
+            .replace(/sqrt/g, '√')
+            .replace(regex, "<sup>$2</sup>")
     }
 }
