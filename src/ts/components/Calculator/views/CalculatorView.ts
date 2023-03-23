e4122ce0a44c95750e49e40b72a3ae06b3bcaa8f
Igor Polischuk <polischuk01@gmail.com>
2023-03-23 12:52:42 +0200

    removed unused imports in view

--- src/ts/components/Calculator/views/CalculatorView.ts
+++ src/ts/components/Calculator/views/CalculatorView.ts
@@ -1,12 +1,7 @@
 import { Calculator } from './components/calculator';
-import { BlockElement } from './../../Elements/BlockElement';
-import { Button } from '../../Elements/Button';
-import { IError } from '../interfaces/ICalculator';
 import { ICalculatorModel, ICalculatorView } from "@components/Calculator/interfaces/ICalculator";
 import { CalculatorObserverEvent } from "../calculator-event";
-import { getMathInput } from './components/CalculatorInput/getMathInput';
-import { getCalculatorButtons } from './components/Keyboard/getCalculatorButtons';
-import { GridContainer } from '@components/Elements/GridContainer';
+
 
 export class CalculatorView implements ICalculatorView {
     private calculator = new Calculator()
