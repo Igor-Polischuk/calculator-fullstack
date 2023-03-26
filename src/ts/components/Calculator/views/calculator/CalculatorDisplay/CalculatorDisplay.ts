887bd6c92f4a8898745573287f362519cb24b127
Igor Polischuk <polischuk01@gmail.com>
2023-03-27 00:52:57 +0300

    implemented error display

--- src/ts/components/Calculator/views/calculator/CalculatorDisplay/CalculatorDisplay.ts
+++ src/ts/components/Calculator/views/calculator/CalculatorDisplay/CalculatorDisplay.ts
@@ -8,40 +8,47 @@ export class CalculatorDisplay {
         this.resultBlock = new DivElement({ classNames: 'calculator__result' })
     }
 
-    renderCalculationResult(result: number | IError[], expression: string) {
-        this.resultBlock.domElement.classList.add('visible')
-        this.resultBlock.removeElement('p')
-        if (typeof result === 'number'){
-            this.renderResultNumber(result, expression)
-        }else{
-            
-        }
-    }
-
-    private renderResultNumber(result: number, expression: string){
-        const resultText = `${this.formatExpression(expression)} = <b>${result}</b>`
-        const p = new Paragraph({text: resultText, classNames: 'result showup'})
-        this.resultBlock.append(p)
-    }
-
-    private renderError(errors: IError[], expressionWithError: string){
-
-    }
-
-    showError(errors: IError[], expressionWithError: string) {
-        // this.resultBlock.domElement.classList.add('visible')
-        // const errorWithDescription = errors.filter(errors => errors.meta.invalidExpressionPart)
-        // const errorText = errorWithDescription[0].meta.invalidExpressionPart
-        // const incorrectParts = errorText?.split('0').filter(symbol => symbol !== '')
-        // const html = incorrectParts?.reduce<string>((acc, error) => {
-        //     return this.replaceSubstringWithHTMLTag(acc, error)
-        // }, expressionWithError)
-
-        // this.resultBlock.domElement.innerHTML = html || ''
-    }
 
     get element() {
         return this.resultBlock
+    }
+
+    renderCalculationResult(result: number | IError[], expression: string) {
+        this.resultBlock.domElement.classList.add('visible')
+        this.resultBlock.removeElement('p')
+        if (typeof result === 'number') {
+            this.renderResultNumber(result, expression)
+        } else {
+            this.renderError(result, expression)
+        }
+    }
+
+    private renderResultNumber(result: number, expression: string) {
+        const resultText = `${this.formatExpression(expression)} = <b>${result}</b>`
+        const p = new Paragraph({ text: resultText, classNames: 'result showup' })
+        this.resultBlock.append(p)
+    }
+
+    private renderError(errors: IError[], expressionWithError: string) {
+        const errorsIndex = this.extractErrorValues(errors, 'errorIndex') as number[]
+        const incorrectParts = this.extractErrorValues(errors, 'invalidExpressionPart').flat() as string[]
+
+        const errorStringByIndex = errorsIndex.reduce<string>((acc, char) => {
+            const errorIndexWrapper = `<span>${expressionWithError[char]}</span>`
+            return this.replaceByIndex(acc, char, errorIndexWrapper)
+        }, expressionWithError)
+        console.log(incorrectParts);
+        
+        const errorStringByParts = incorrectParts.reduce<string>((acc, incorrectPart) => {
+            return this.replaceSubstringWithHTMLTag(acc, incorrectPart)
+        }, errorStringByIndex)
+        
+        const p = new Paragraph({ text: errorStringByParts, classNames: 'error' })
+        this.resultBlock.append(p)
+    }
+
+    private extractErrorValues(errors: IError[], param: keyof IError['meta']): Array<number | string>{
+        return errors.map(error => error.meta[param]).filter(item => item !== undefined) as Array<number | string>
     }
 
     private formatExpression(expression: string) {
@@ -58,7 +65,7 @@ export class CalculatorDisplay {
 
     private replaceSubstringWithHTMLTag(input: string, substring: string): string {
         const escapedSubstring = substring.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-        const tag = '<span class="error">' + substring + '</span>';
+        const tag = '<span>' + substring + '</span>';
         const regex = new RegExp(escapedSubstring, 'g');
         return input.replace(regex, tag);
     }
