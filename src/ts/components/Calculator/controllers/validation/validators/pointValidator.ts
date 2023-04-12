f593b6489e5f84993deafea5ac96d6eeee25348a
Igor Polischuk <polischuk01@gmail.com>
2023-04-12 14:54:52 +0300

    remove console log from point validation

--- src/ts/components/Calculator/controllers/validation/validators/pointValidator.ts
+++ src/ts/components/Calculator/controllers/validation/validators/pointValidator.ts
@@ -5,8 +5,7 @@ import { getSubstringsIndexes } from "../helpers/getSubstringsIndexes";
 
 export function pointValidator(expression: string): IError | undefined {
     const numberWithSeveralPoints = expression.match(regexPatterns.DOUBLE_POINTS_IN_NUMBER)
-    console.log(numberWithSeveralPoints);
-    
+
     if (numberWithSeveralPoints) return {
         message: Error.NumberPointError,
         errorPlace: getSubstringsIndexes(numberWithSeveralPoints, expression)
