40b59b3dc4b697d261b7cae2f2113e5d5716b6d2
Igor Polischuk <polischuk01@gmail.com>
2023-04-28 12:19:05 +0300

    added validations middlewares with express-validator

---
+++ server/src/middlewares/validation.ts
@@ -0,0 +1,27 @@
+import { AppError } from "@errors/AppError";
+import { ResponseFormatter } from "@utils/ResponseFormatter";
+import { NextFunction, Request, Response } from "express";
+import { ValidationChain, validationResult } from "express-validator";
+
+export function validation(validations: ValidationChain[]) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        for (let validation of validations) {
+            const result = await validation.run(req);
+            if (!result.isEmpty()) break;
+        }
+
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            return next();
+        }
+
+        const error = AppError.getErrorFrom(errors.array()[0].msg)
+
+        const JSONResponse = new ResponseFormatter({
+            status: error.status,
+            error
+        }).json()
+
+        res.status(error.status).send(JSONResponse);
+    };
+};
--- server/src/routers/calculatorRouter.ts
+++ server/src/routers/calculatorRouter.ts
@@ -1,9 +1,25 @@
-import { getHistory, getOperations, getResult } from "@controllers/calculatorController";
 import { Router } from "express";
+import { query } from "express-validator";
+
+import { getHistory, getOperations, getResult } from "@controllers/calculatorController";
+import { validation } from "middlewares/validation";
+import { AppError } from "@errors/AppError";
+import { ErrorType } from "@errors/error-type";
+import { validateExpression } from "@services/calculatorService/expressionValidation/validateExpression";
 
 const calculatorRouter = Router()
 
-calculatorRouter.get('/resultOF', getResult)
+calculatorRouter.get('/resultOF', [validation([
+    query('expression')
+        .notEmpty()
+        .withMessage(new AppError({
+            status: 400,
+            message: 'Missing required parameter: expression',
+            type: ErrorType.MissingParameter
+        }))
+        .custom(validateExpression)
+]), getResult])
+
 calculatorRouter.get('/operations', getOperations)
 calculatorRouter.get('/history', getHistory)
 
