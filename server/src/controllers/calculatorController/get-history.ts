37a3dcafc2970e721124c25b78f4cda17a9b8810
Igor Polischuk <polischuk01@gmail.com>
2023-05-01 21:19:18 +0300

    implemented getHistory

--- server/src/controllers/calculatorController/get-history.ts
+++ server/src/controllers/calculatorController/get-history.ts
@@ -1,5 +1,10 @@
+import { ResponseFormatter } from "@utils/ResponseFormatter";
 import { Request, Response } from "express";
+import { JsonDB } from "repositories/history-repository/JsonDB";
 
-export function getHistory(req: Request, res: Response): void {
-    res.send('Getting history...')
+export async function getHistory(req: Request, res: Response): Promise<void> {
+    const db = new JsonDB('./src/data/history.json')
+    const history = await db.getAll()
+    const responseJSON = new ResponseFormatter({ data: { history } }).json()
+    res.send(responseJSON)
 }
